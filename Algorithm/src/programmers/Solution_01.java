fef92d88e3b63d0f679be6fbd4f5f1e92f22eb61
LG <LG@192.168.219.105>
2022-03-02 01:52:57 +0900

    Summer/Winter Coding(~2018) > 소수 만들기

--- Algorithm/src/programmers/Solution_01.java
+++ Algorithm/src/programmers/Solution_01.java
@@ -1,13 +1,14 @@
 package programmers;
 
 // Summer/Winter Coding(~2018) > 소수 만들기
+// nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return
 public class Solution_01 {
 	
 	public static void main(String[] args) {
 		int[] nums = {1,2,3,4}; // 숫자들이 들어있는 배열
 		System.out.println(solution(nums));
 	}
-	// nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return
+	
 	public static int solution(int[] nums) {
 		int answer = 0; // 소수가 되는 경우의 개수
 		int sum = 0; // 서로 다른 숫자 3개의 합
@@ -24,6 +25,7 @@ public class Solution_01 {
 		}
 		return answer;
 	}
+	
 	// 소수 체크 함수
 	private static boolean isPrime(int sum) {
 		for (int i = 2; i <= Math.sqrt(sum); i++) { // 0과 1은 소수 아님
