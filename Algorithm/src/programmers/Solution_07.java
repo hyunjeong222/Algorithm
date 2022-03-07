2eea85f823a38fc3c86de5f1d55c882978869356
LG <LG@192.168.219.105>
2022-03-08 02:21:17 +0900

    오픈채팅방

--- Algorithm/src/programmers/Solution_07.java
+++ Algorithm/src/programmers/Solution_07.java
@@ -13,12 +13,12 @@ public class Solution_07 {
 		String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
 		System.out.println(Arrays.toString(solution(record)));
 	}
-	
+
 	public static String[] solution(String[] record) {
-        String[] answer = {};
-        ArrayList<String> list = new ArrayList<String>(); // 기록을 남길 리스트
-        HashMap<String, String> map = new LinkedHashMap<String, String>(); // 유저 아이디, 닉네임
-        for (String r : record) {
+		String[] answer = {};
+		ArrayList<String> list = new ArrayList<String>(); // 기록을 남길 리스트
+		HashMap<String, String> map = new LinkedHashMap<String, String>(); // 유저 아이디, 닉네임
+		for (String r : record) {
 			String[] arr = r.split(" ");
 			if (arr[0].equals("Enter")) {
 				list.add(arr[1] + "님이 들어왔습니다.");
@@ -29,16 +29,16 @@ public class Solution_07 {
 				list.add(arr[1] + "님이 나갔습니다.");
 			}
 		}
-        // System.out.println(list);
-        // [uid1234님이 들어왔습니다., uid4567님이 들어왔습니다., uid1234님이 나갔습니다., uid1234님이 들어왔습니다.]
-        // System.out.println(map);
-        answer = new String[list.size()]; // 배열 크기 정의
-        for (int i = 0; i < list.size(); i++) {
+		// System.out.println(list);
+		// [uid1234님이 들어왔습니다., uid4567님이 들어왔습니다., uid1234님이 나갔습니다., uid1234님이 들어왔습니다.]
+		// System.out.println(map);
+		answer = new String[list.size()]; // 배열 크기 정의
+		for (int i = 0; i < list.size(); i++) {
 			int idx = list.get(i).indexOf("님"); // 유저 아이디만 자르기 위한 위치값
 			String id = list.get(i).substring(0,idx); // 유저 아이디
 			// System.out.println(id);
 			answer[i] = map.get(id) + list.get(i).substring(idx); // 유저 아이디를 닉네임으로 변경 + 상태
 		}
-        return answer;
-    }
+		return answer;
+	}
 }
