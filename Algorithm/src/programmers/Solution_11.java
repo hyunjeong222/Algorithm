f60ae6058680688c207fbb117c5ec315ccddf36f
LG <LG@192.168.219.105>
2022-03-17 02:10:46 +0900

    내적

--- Algorithm/src/programmers/Solution_11.java
+++ Algorithm/src/programmers/Solution_11.java
@@ -13,6 +13,7 @@ public class Solution_11 {
 	public static int solution(int[] a, int[] b) {
         int answer = 0;
         for (int i = 0; i < a.length; i++) {
+        	// a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
         	answer += a[i] * b[i];
 		}
         return answer;
