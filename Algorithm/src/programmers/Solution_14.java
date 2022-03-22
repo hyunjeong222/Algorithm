5d47ef7790dd8d7d13d4c974ef733cfcbd5361ea
LG <LG@192.168.219.105>
2022-03-23 01:21:23 +0900

    괄호 변환

--- Algorithm/src/programmers/Solution_14.java
+++ Algorithm/src/programmers/Solution_14.java
@@ -12,10 +12,10 @@ public class Solution_14 {
 	}
 
 	public static String solution(String p) {
-        // String answer = "";
-        if (check(p)) return p;
-        return dfs(p);
-    }
+		// String answer = "";
+		if (check(p)) return p;
+		return dfs(p);
+	}
 
 	private static String dfs(String p) {
 		if (p.equals("")) return p; // 이미 "올바른 괄호 문자열"이라면 그대로 return
@@ -26,7 +26,7 @@ public class Solution_14 {
 		for (int i = 0; i < p.length(); i++) {
 			if (p.charAt(i) == '(') left++;
 			else right++;
-			
+
 			if ((left != 0 && right != 0) && left == right) { // 올바른 괄호 쌍이라면	
 				u = p.substring(0,i+1);
 				if (i != p.length() - 1) {
@@ -36,19 +36,23 @@ public class Solution_14 {
 			}
 		}
 		if (!check(u)) { // u가 올바른 괄호 쌍이 아니라면
-			String temp = "(" + dfs(v);
-			temp += ")";
+			String temp = "(" + dfs(v); // 빈 문자열에 첫 번째 문자로 '(' 붙이고 문자열 v에 대해 재귀
+			temp += ")"; // ')' 다시 붙이기
+			// u의 첫 번째와 마지막 문자 제거
 			u = u.substring(1, u.length()-1);
+			// 나머지 문자열의 괄호 방향 뒤집기
 			u = u.replace("(", ".");
 			u = u.replace(")", "(");
 			u = u.replace(".", ")");
+			// 문자열 붙이기
 			temp += u;
 			return temp;
 		} else { // 올바른 괄호라면
-			return u + dfs(v);
+			return u + dfs(v); // u에 v에 대해 재귀한 문자열 붙이기
 		}
 	}
 
+	// 올바른 괄호 판별 
 	private static boolean check(String p) {
 		Stack<Character> stack = new Stack<>();
 		for (int i = 0; i < p.length(); i++) {
@@ -63,5 +67,5 @@ public class Solution_14 {
 		if (!stack.isEmpty()) return false;
 		return true;
 	}
-	
+
 }
